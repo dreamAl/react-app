047461bd6cccda2c88a0c5db55d5fda3aa2b66cb
my-app <1093993119@qq.com>
2019-10-12 07:23:48 +0800

    PUBLISH

--- src/pages/message.js
+++ src/pages/message.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import TopBar from '../components/topBar/topBar'
+import  '../style/message.css'
 export default class message extends Component {
     render() {
         return (
             <div>
-                消息
+                 <TopBar/>
+                 <p className="ptext">你还没有消息呢</p>
             </div>
         )
     }
--- src/pages/publish.js
+++ src/pages/publish.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import Img from '../img/ban.png'
+import '../style/publish.css'
 export default class message extends Component {
     render() {
         return (
-            <div>
-                发布
+            <div className="box">
+                <img src={Img} alt=""/>
             </div>
         )
     }
